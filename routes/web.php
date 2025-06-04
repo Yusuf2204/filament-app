e649cc4057762d05527a46d891925c17eb2686e8
Thoriq Hafidzuzumar <72118661+thoriqhafidz12@users.noreply.github.com>
2025-06-04 17:22:50 +0900

    update mastering view

--- routes/web.php
+++ routes/web.php
@@ -1,7 +1,9 @@
 <?php
 
+use Filament\Http\Responses\Auth\Contracts\LoginResponse;
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
-    return view('welcome');
+    // return view('welcome');
+    return app(LoginResponse::class);
 });
